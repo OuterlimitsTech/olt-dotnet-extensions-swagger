a040ab2ad54565d0dc2a6cf19f8cdbaf290d0b11
Chris Straw <chris.straw@outerlimitstech.com>
2022-04-04 08:23:05 -0400

    code cleanup

---
+++ src/OLT.Extensions.SwaggerGen/ArgBuilder/OltSwaggerContactArgs.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace OLT.Extensions.SwaggerGen
+{
+    public abstract class OltSwaggerContactArgs<T> : OltSwaggerLicenseArgs<T>
+        where T : OltSwaggerTitleArgs<T>
+    {
+
+        internal OpenApiContact? Contact { get; set; }
+
+        protected OltSwaggerContactArgs()
+        {
+        }
+
+        /// <summary>
+        /// API Contact
+        /// </summary>
+        /// <param name="value"><see cref="OpenApiContact"/></param>
+        /// <returns><typeparamref name="T"/></returns>
+        public T WithApiContact(OpenApiContact value)
+        {
+            this.Contact = value;
+            return (T)this;
+        }
+    }
+}
--- src/OLT.Extensions.SwaggerGen/ArgBuilder/OltSwaggerTitleArgs.cs
+++ src/OLT.Extensions.SwaggerGen/ArgBuilder/OltSwaggerTitleArgs.cs
@@ -1,51 +1,7 @@
-﻿using Microsoft.OpenApi.Models;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace OLT.Extensions.SwaggerGen
 {
-    public abstract class OltSwaggerLicenseArgs<T> : OltSwaggerXmlCommentArgs<T>
-       where T : OltSwaggerLicenseArgs<T>
-    {
-
-        internal OpenApiLicense? License { get; set; }
-
-        protected OltSwaggerLicenseArgs()
-        {
-        }
-
-        /// <summary>
-        /// API Licensing
-        /// </summary>
-        /// <param name="value"><see cref="OpenApiLicense"/></param>
-        /// <returns><typeparamref name="T"/></returns>
-        public T WithApiLicense(OpenApiLicense value)
-        {
-            this.License = value;
-            return (T)this;
-        }
-    }
-
-    public abstract class OltSwaggerContactArgs<T> : OltSwaggerLicenseArgs<T>
-        where T : OltSwaggerTitleArgs<T>
-    {
-
-        internal OpenApiContact? Contact { get; set; }
-
-        protected OltSwaggerContactArgs()
-        {
-        }
-
-        /// <summary>
-        /// API Contact
-        /// </summary>
-        /// <param name="value"><see cref="OpenApiContact"/></param>
-        /// <returns><typeparamref name="T"/></returns>
-        public T WithApiContact(OpenApiContact value)
-        {
-            this.Contact = value;
-            return (T)this;
-        }
-    }
 
     public abstract class OltSwaggerTitleArgs<T> : OltSwaggerContactArgs<T>
         where T : OltSwaggerTitleArgs<T>
